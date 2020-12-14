815e8b186666c689d99e828680363c512ae31e22
Ubuntu <ubuntu@ip-172-31-0-236.eu-west-1.compute.internal>
2020-12-14 18:41:07 +0000

    pruebas

--- miproyecto/src/main/java/es/curso/App.java
+++ miproyecto/src/main/java/es/curso/App.java
@@ -10,4 +10,15 @@ public class App
     {
         System.out.println( "Hola Jenkins!" );
     }
+    
+    public static int doblar( int numero )
+    {
+        return numero * 2;
+    }
+    
+    public static int mayor( int numero1, int numero2 )
+    {
+        return numero1 > numero2 ? numero1 : numero2;
+    }
+    
 }
--- miproyecto/src/test/java/es/curso/AppTest.java
+++ miproyecto/src/test/java/es/curso/AppTest.java
@@ -17,4 +17,38 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void pruebaDoblar()
+    {
+        int resultado = App.doblar(5);
+        assertEquals( resultado, 10 );
+    }
+    
+    @Test
+    public void pruebaDoblarCero()
+    {
+        int resultado = App.doblar(0);
+        assertEquals( resultado, 0 );
+    }
+    
+    @Test
+    public void pruebaDoblarNegativo()
+    {
+        int resultado = App.doblar(-5);
+        assertEquals( resultado, -10 );
+    }
+    
+    @Test
+    public void pruebaMayor1()
+    {
+        int resultado = App.mayor(5, 10)
+        assertEquals( resultado, 10 );
+    }
+    @Test
+    public void pruebaMayor2()
+    {
+        int resultado = App.mayor(10, 5)
+        assertEquals( resultado, 10 );
+    }
 }
